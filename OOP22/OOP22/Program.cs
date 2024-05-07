e4718e5005fd6b6fe488d7fbbbf085964731bfd8
ChingizbekXurramov <chingizbekxurramov@gmail.com>
2024-05-07 00:48:47 -0700

    OOP_22

---
+++ OOP22/OOP22/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace OOP22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Vvodeti komponenti pervomu vectoru (x y)=>");
+            string[] v1Input = Console.ReadLine().Split();
+            Vector v1 = new Vector(int.Parse(v1Input[0]), int.Parse(v1Input[1]));
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Vvodeti komponenti vtoromu vectoru  (x y)=>");
+            string[] v2Input = Console.ReadLine().Split();
+            Vector v2 = new Vector(int.Parse(v2Input[0]), int.Parse(v2Input[1]));
+            
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Sravniniy=>");
+            Console.WriteLine($"1 vector ravno 2 ==>  {v1 == v2}");
+            Console.WriteLine($"1 vector ne ravno 2 ==> {v1 != v2}");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nSkalyarnoy umnojiniy=>");
+            Vector scaledVector = 3 * v1;
+            Console.WriteLine($"({scaledVector.X}, {scaledVector.Y})");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nUmnojiniy =>:");
+            int dotProduct = v1 * v2;
+            Console.WriteLine(dotProduct);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nVectornoy vechitaniy=>");
+            Vector subtractionVector = v2 - v1;
+            Console.WriteLine($"({subtractionVector.X}, {subtractionVector.Y})");
+        }
+    }
+}
+
---
+++ OOP22/OOP22/Vector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP22
+{
+    internal class Vector
+    {
+        private int _x;
+        private int _y;
+
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public Vector(int x=0, int y=0)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static bool operator ==(Vector v1, Vector v2)
+        {
+            return v1.X == v2.X && v1.Y == v2.Y;
+        }
+
+        public static bool operator !=(Vector v1, Vector v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public static Vector operator *(int scalar, Vector v)
+        {
+            return new Vector(scalar * v.X, scalar * v.Y);
+        }
+
+        public static int operator *(Vector v1, Vector v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y;
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+    }
+}
